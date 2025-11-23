1afb28b1684a46d12cdce3cf2697054fdf4ea252
gauravsahu1990 <gaurav.sahu@nagarro.com>
2025-11-23 15:48:09 +0530

    Update folder path for API upload

--- tourism_project/hosting/hosting.py
+++ tourism_project/hosting/hosting.py
@@ -3,7 +3,7 @@ import os
 
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
-    folder_path="tourism_package_prediction/deployment",     # the local folder containing your files
+    folder_path="tourism_project/deployment",     # the local folder containing your files
     repo_id="gauravsahu1990/Tourism-Package-Prediction",          # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
